7a260e289f2494bb7b2c1a6f61750e78c5e33913
Zhantai <Zhantai.Ismailov@gmail.com>
2021-07-07 14:10:02 +0600

    added homepage and profile page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def homepage(request):
+    return render(request, 'core/index.html')
+
+
+def profile(request):
+    user = request.user
+    profile_object = user.profile
+    return render(request, 'core/profile.html', {'profile': profile_object})
