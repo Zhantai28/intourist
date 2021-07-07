cec1f476172ad7177261b8f22b4ab86e78987723
Zhantai <Zhantai.Ismailov@gmail.com>
2021-07-07 16:24:30 +0600

    added forms, base and CRUD functions

--- places/urls.py
+++ places/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import *
+from .views import place, places, create_place, edit_place, delete_place
 
 app_name = 'places'
 
 urlpatterns = [
-    path('', places, name='places-list')
+    path('', places, name='places-list'),
+    path('create/', create_place, name="create-place"),
+    path('<int:id>/', place, name='place'),
+    path('<int:id>/edit/', edit_place, name='edit-place'),
+    path('<int:id>/delete/', delete_place, name='delete-place'),
 ]
