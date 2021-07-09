0f05a77bb9955aed0353d14587f3cd40f1990e95
Zhantai <Zhantai.Ismailov@gmail.com>
2021-07-09 11:22:08 +0600

    added some tests

--- places/tests.py
+++ places/tests.py
@@ -1,10 +1,34 @@
-from django.http import response
 from django.test import TestCase
 from django.urls import reverse
+from django.db.models import QuerySet
+from places.models import Place
+from .factories import PlaceFactory
 
 
 class PlacesListTestCase(TestCase):
     def test_open_list_success(self):
-        url = reverse('places-list')
+        place_1 = PlaceFactory(name="Cool place", description="Visit any time")
+        place_2 = PlaceFactory()
+
+        url = reverse('places:places-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+        places = response.context.get('places')
+        self.assertIsInstance(places, QuerySet)
+
+        place_2_db = Place.objects.get(name="Place number 1")
+        self.assertEqual('Location - 1', places[1].location)
+        self.assertEqual(places[0].description, "Visit any time")
+
+
+class PalceCreateTestCase(TestCase):
+    def test_create_place_success(self):
+        url = reverse('places:create-place')
+        data = {
+            'name': 'Issyk-Kul',
+            'location': 'Kara-Kol region',
+            'description': 'Lake in KG'
+        }
+        response = self.client.post(url, data)
+        place = Place.objects.last()
+        self.assertEqual(place.name, 'Issyk-Kul')
